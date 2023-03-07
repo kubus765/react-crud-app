4dafb83c4e5d496b131043c025151549e282e963
kubus765 <117770683+kubus765@users.noreply.github.com>
2023-03-07 18:02:59 +0100

    Final version email validation

--- src/Contacts.js
+++ src/Contacts.js
@@ -62,7 +62,7 @@ const Contacts = () => {
     ]
     // regex for email verification
     const isValidEmail = (email) => {
-        const regex = /^\.?[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return regex.test(email);
       }
     // regex for password verification
