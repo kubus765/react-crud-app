0da7e2dff881580c8d4014d99e8277c186af2134
kubus765 <117770683+kubus765@users.noreply.github.com>
2023-03-07 22:05:34 +0100

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-/*
-const data = [ {
-  "id": 1,
-  "name": "Water",
-  "surname": "Fire",
-  "email": "waterfire@mail.com",
-  "password": "waterfirWeFire@#@as2312",
-  "phone": "123123123",
-  "category": "asdasdasdasd",
-  "dateOfBirth": "1999-09-09"
-}]
-*/
 
 // This code defines a functional React component called App, 
 // which renders a div element with the class name "App" and includes another component called Contacts. 
@@ -34,7 +22,6 @@ function App() {
         getData();
     }, [])
     
-
   const getData = () => {
       axios.get('https://localhost:7275/api/Contact')
           .then((result) => {
@@ -44,70 +31,6 @@ function App() {
               console.log(error)
           })
   }
-
-/*
-
-    // This code sends a PUT request to an API endpoint to update a contact's information, using the axios library. 
-    // It displays a success message using the toast library if the request succeeds, or an error message if it fails.
-  const handleUpdate = () => {
-    const url = `https://localhost:7275/api/Contact/${editID}`;
-    const data = {
-        "id": editID,
-        "name": editName,
-        "surname": editSurname,
-        "email": editEmail,
-        "password": editPassword,
-        "phone": editPhone,
-        "category": editCategory,
-        "dateOfBirth": editDateOfBirth
-    }
-    axios.put(url, data)
-        .then(() => {
-            getData();
-            if(!isFormValid({name:editName,surname:editSurname,email:editEmail,password:editPassword,phone:editPhone,category:editCategory,dateOfBirth:editDateOfBirth})) return;
-            toast.success("Contact has been updated.");
-            handleClose();
-        }).catch((error) => {
-            toast.error(error);
-        })
-        
-}
-
-  
-
-    // This code handles the editing of a contact 
-    // by retrieving its information from the API endpoint and updating the state variables 
-    // used to populate the input fields in the modal dialog.
-
-  const handleEdit = (id) => {
-    handleShow();
-    axios.get(`https://localhost:7275/api/Contact/${id}`)
-        .then((result) => {
-            setEditId(result.data.id);
-            setEditName(result.data.name);
-            setEditSurname(result.data.surname);
-            setEditEmail(result.data.email);
-            setEditPassword(result.data.password);
-            setEditCategory(result.data.category);
-            setEditPhone(result.data.phone);
-            setEditDateOfBirth(result.data.dateOfBirth);
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-}
-
-const clear = () => {
-  setName('');
-  setSurname('');
-  setEmail('');
-  setPassword('');
-  setPhone('');
-  setCategory('');
-  setDateOfBirth('');
-}
-
-*/
 
     // This code sends a POST request to add a new contact
     // updates the contact list and clears input fields 
@@ -136,11 +59,9 @@ const handleSave = ({name,surname,email,password,phone,category,dateOfBirth}) =>
       })
 }
 
-
     // This code sends a DELETE request to remove a contact with the given ID,
     // displays a confirmation dialog before deleting
     // updates the contact list and displays a toast message after a successful response.
-
 
 const handleDelete = (id) => {
   if (window.confirm("Are you sure you want to delete this contact?") == true) {
@@ -156,7 +77,6 @@ const handleDelete = (id) => {
         })
 }
 }
-
 
   return (
     <div className="App">
